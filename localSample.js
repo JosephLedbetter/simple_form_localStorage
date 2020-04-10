a1cd2e08820f76b82691ff07ec6981dedd96adc3
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-04-10 10:29:54 -0400

    simple rendition of a form that captures the first and last name, then saves the items to local storage

---
+++ localSample.js
@@ -0,0 +1,21 @@
+document.querySelector('form').addEventListener('submit', function(e){
+    const firstName = document.getElementById('first_name').value;
+    const lastName = document.getElementById('last_name').value;
+
+    let listOfNames;
+
+    if (localStorage.getItem('listOfNames') === null){
+        listOfNames = [];
+    } else {
+        listOfNames = JSON.parse(localStorage.getItem('listOfNames'));
+    }
+
+    listOfNames.push(`${firstName} ${lastName}`);
+
+    localStorage.setItem('listOfNames', JSON.stringify(listOfNames));
+
+
+    alert('Full name added successfully')
+    console.log(firstName, lastName);
+    e.preventDefault()
+})
